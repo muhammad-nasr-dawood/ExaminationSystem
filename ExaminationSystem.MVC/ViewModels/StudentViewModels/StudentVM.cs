e78c67c76bee54ef602b05d0055f3d334e706185
Abanoub <ali@gmail.com>
2025-04-14 17:54:06 +0200

    Student Index is done except UI

---
+++ ExaminationSystem.MVC/ViewModels/StudentViewModels/StudentVM.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ExaminationSystem.MVC.ViewModels.StudentViewModels
+{
+  public class StudentVM
+  {
+	[Display(Name ="Full Name")]
+	public string FullName { get; set; }
+	public string Email { get; set; }
+	public string Gender { get; set; }
+	public bool IsActive { get; set; }
+	public string Faculty { get; set; }
+	public string ImageUrl { get; set; }
+
+  }
+}
