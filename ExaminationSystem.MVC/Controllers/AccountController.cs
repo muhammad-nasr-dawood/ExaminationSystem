440d0ead89307e4c33250a2d3b2109abf82f962a
mo7amednasr72 <mohamednasr7222@gmail.com>
2025-04-29 20:50:43 +0300

    feat: User Profile done except for the student datatable waiting for bob to integrate

--- ExaminationSystem.MVC/Controllers/AccountController.cs
+++ ExaminationSystem.MVC/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
 using AspNetCoreGeneratedDocument;
+using ExaminationSystem.Core.Consts;
+using ExaminationSystem.Core.Models;
 using ExaminationSystem.MVC.Services;
 using ExaminationSystem.MVC.ViewModels.AuthViewModels;
+using ExaminationSystem.MVC.ViewModels.BranchViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,12 +15,28 @@ namespace ExaminationSystem.MVC.Controllers;
 public class AccountController : Controller
 {
     private readonly IAccountService _accountService;
-    public AccountController(IAccountService accountService)
+	private readonly IStaffService _staffService;
+	private readonly IMapper _mapper;
+    public AccountController(IAccountService accountService, IStaffService staffService, IMapper mapper)
     {
         _accountService = accountService;
-
+		_staffService = staffService;
+		_mapper = mapper;
     }
-    public IActionResult Index()
+
+	public IActionResult Index()
+	{
+	  ViewBag.Branches = _mapper.Map<List<BranchDisplayViewModel>>(_staffService.UnitOfWork.BranchesRepo.GetAll());
+	  ViewBag.Departments = _staffService.UnitOfWork.DepartmentRepo.GetAll();
+
+	  ViewBag.Locations = _staffService.UnitOfWork.LocationRepo.GetAll();
+	  ViewBag.Courses = _staffService.UnitOfWork.CoursesRepo.GetAll();
+	  var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+	  var account = _accountService.GetAccount(long.Parse(userId));
+	  return View(account);
+	}
+
+    public IActionResult AccountSettings()
     {
         string? userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
@@ -52,7 +71,7 @@ public class AccountController : Controller
 	return Json(new
 	{
 	  success = success,
-	  redirectUrl = Url.Action("Index", "Staff")
+	  redirectUrl = Url.Action("Index")
 	});
   }
 
@@ -163,8 +182,68 @@ public class AccountController : Controller
 
 
 
+  [HttpPost]
+  public IActionResult GetAllRegisteredCourses()
+  {
+	try
+	{
+	  var draw = int.Parse(Request.Form["draw"].FirstOrDefault() ?? "1");
+	  var start = int.Parse(Request.Form["start"].FirstOrDefault() ?? "0");
+	  var length = int.Parse(Request.Form["length"].FirstOrDefault() ?? "10");
+
+	  var searchValue = Request.Form["search[value]"].FirstOrDefault();
+	  var filterStatus = bool.TryParse(Request.Form["status"].FirstOrDefault(), out var fStatus) ? fStatus : (bool?)null;
+	  var branchId = int.TryParse(Request.Form["branchId"].FirstOrDefault(), out var bId) ? bId : (int?)null;
+	  var deptId = int.TryParse(Request.Form["DeptId"].FirstOrDefault(), out var dId) ? dId : (int?)null;
+	  var crsId = int.TryParse(Request.Form["courseId"].FirstOrDefault(), out var cId) ? cId : (int?)null;
+
+	  var staffSsn = long.Parse( User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+	  var orderColumnIndex = int.Parse(Request.Form["order[0][column]"].FirstOrDefault() ?? "0");
+	  var orderDir = Request.Form["order[0][dir]"].FirstOrDefault() ?? "asc";
+
+	  // Match column index to actual property names in your model or query
+	  string[] columnNames = { "Course", "IsInstructorCurrentBranch", "StartingDate", "EndingDate" };
+	  string orderBy = columnNames.ElementAtOrDefault(orderColumnIndex) ?? "Course";
 
 
-  
+	  int pageNumber = (start / length) + 1;
+	  int pageSize = length;
+
+	  var courseResult = _staffService.FindAllRegisteredCourses(
+		  pageNumber: pageNumber,
+		  pageSize: pageSize,
+		  branchIdFilter: branchId,
+		  departmentIdFilter: deptId,
+		  courseFilter: crsId,
+		  StaffSnn: staffSsn,
+		  status: filterStatus,
+		  columnOrderBy: orderBy,
+		  orderByDirection: orderDir == "asc" ? OrderBy.Ascending : OrderBy.Descending,
+		  searchTerm: searchValue
+	  );
+
+	  return Json(new
+	  {
+		draw = draw,
+		recordsTotal = courseResult.TotalItemsInTable,
+		recordsFiltered = courseResult.TotalFilteredItems,
+		data = courseResult.Items
+	  });
+	}
+	catch (Exception ex)
+	{
+	  // You can optionally log the exception here for debugging
+
+	  return Json(new
+	  {
+		draw = 1,
+		recordsTotal = 0,
+		recordsFiltered = 0,
+		data = new List<TeachingDisplayViewModel>()
+	  });
+	}
+  }
+
 
 }
