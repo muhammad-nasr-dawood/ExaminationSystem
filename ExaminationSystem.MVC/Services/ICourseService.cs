7dfe94d420980d24561c82d31b9e3adcaf7a4feb
Ahmed-Abbas20 <ahmed.m.abbas200200@gmail.com>
2025-05-09 17:16:36 +0300

    Fix:TopicFunAdded

--- ExaminationSystem.MVC/Services/ICourseService.cs
+++ ExaminationSystem.MVC/Services/ICourseService.cs
@@ -25,5 +25,6 @@ namespace ExaminationSystem.MVC.Services
 	public object AddCourse(CourseAddEditViewModel model);
 	public object EditCourse(CourseAddEditViewModel model);
 	public List<CourseDisplayViewModel> GetAll();
+	public List<Topic> GetTopicsByCourse(int courseId);
   }
 }
